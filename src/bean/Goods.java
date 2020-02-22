a0b2292ceb7327061b360552f50f42a87bcf8a56
ZengChaoYuan <1053160284@qq.com>
2020-02-22 09:53:02 +0800

    2020222(4):建好Goods类

--- src/bean/Goods.java
+++ src/bean/Goods.java
@@ -1,5 +1,33 @@
 package bean;
 
 public class Goods {
+	private int goodsId;
+	private String goodsName;
 
+	public Goods(int goodsId, String goodsName) {
+		super();
+		this.goodsId = goodsId;
+		this.goodsName = goodsName;
+	}
+
+	public Goods() {
+		super();
+	}
+
+	public int getGoodsId() {
+		return goodsId;
+	}
+
+	public void setGoodsId(int goodsId) {
+		this.goodsId = goodsId;
+	}
+
+	public String getGoodsName() {
+		return goodsName;
+	}
+
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+//   
 }

76b07c24f0c55845711e5e5b7503f21033b9f3e9
ZengChaoYuan <1053160284@qq.com>
2020-02-22 09:55:49 +0800

    2020222(5):完善好City类

--- src/bean/City.java
+++ src/bean/City.java
@@ -1,5 +1,33 @@
 package bean;
 
 public class City {
+	private int cityId;
+	private String cityName;
+
+	public City(int cityId, String cityName) {
+		super();
+		this.cityId = cityId;
+		this.cityName = cityName;
+	}
+
+	public City() {
+		super();
+	}
+
+	public int getCityId() {
+		return cityId;
+	}
+
+	public void setCityId(int cityId) {
+		this.cityId = cityId;
+	}
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
 
 }

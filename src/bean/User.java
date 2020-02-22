a5de538ddcf839341de4508a8e641f3c20ef8923
ZengChaoYuan <1053160284@qq.com>
2020-02-22 09:58:58 +0800

    2020222(6):完善User类

--- src/bean/User.java
+++ src/bean/User.java
@@ -1,5 +1,63 @@
 package bean;
 
 public class User {
+	private int userId;
+	private String userName;
+	private String password;
+	private String realName;
+	private int sex;
+
+	public User(int userId, String userName, String password, String realName, int sex) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.password = password;
+		this.realName = realName;
+		this.sex = sex;
+	}
+
+	public User() {
+		super();
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRealName() {
+		return realName;
+	}
+
+	public void setRealName(String realName) {
+		this.realName = realName;
+	}
+
+	public int getSex() {
+		return sex;
+	}
+
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
 
 }
